c05895f854c7196a2b37669928ab3e4f97a6df77
anthonymnf <anthonymnf30@gmail.com>
2025-02-19 20:24:47 -0300

    :sparkles: chore: Changed Next.js configurations for image

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "u9a6wmr3as.ufs.sh",
+        pathname: "/f/**", // Ajuste conforme necessário para permitir diferentes caminhos
+      },
+    ],
+  },
 };
 
 export default nextConfig;
